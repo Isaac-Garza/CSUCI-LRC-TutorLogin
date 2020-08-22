a0a3a90b229089aad05a89dee3955544bfbe9ef8
ICHeadache <55867190+ICHeadache@users.noreply.github.com>
2020-08-22 16:16:03 -0700

    LogIn Navigation

--- app/src/main/java/com/example/tutorlogin/TutorHub.java
+++ app/src/main/java/com/example/tutorlogin/TutorHub.java
@@ -2,13 +2,25 @@ package com.example.tutorlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TutorHub extends AppCompatActivity {
+public class TutorHub extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor_hub);
+        Button addTutorButton = findViewById(R.id.add_button);
+
+        addTutorButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i =
+
     }
 }
