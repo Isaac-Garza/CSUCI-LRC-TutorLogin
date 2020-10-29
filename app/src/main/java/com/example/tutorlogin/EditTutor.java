053c3367efb1244d1ca54d7e7889a169c7fedd9c
ICHeadache <55867190+ICHeadache@users.noreply.github.com>
2020-10-29 09:26:12 -0700

    Edit Item to List

--- app/src/main/java/com/example/tutorlogin/EditTutor.java
+++ app/src/main/java/com/example/tutorlogin/EditTutor.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class EditTutor extends AppCompatActivity {
 
     TextView editTutorTitle;
+    Button editTutor;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +27,21 @@ public class EditTutor extends AppCompatActivity {
         String selectedTutorTitle = intent.getStringExtra("TutorName");
         editTutorTitle = findViewById(R.id.selected_tutor);
         editTutorTitle.setText(selectedTutorTitle);
+
+//        editTutor.setOnClickListener(new View.OnClickListener()
+//        {
+//
+//            @Override
+//            public void onClick(View v) {
+//
+//                rootNode = FirebaseDatabase.getInstance();
+//                reference = rootNode.getReference("Tutor");
+//                reference.child(tutorModel.getId()).setValue(tutorModel);
+//                success = true;
+//
+//
+//            }
+//        });
 
 
     }
