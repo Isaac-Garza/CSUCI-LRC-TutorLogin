b24ecb2db4ee9fd0509e94a6fb0ed5993d7f9efe
Isaac-Garza <Igarza957@yahoo.com>
2020-11-11 14:37:35 -0800

    Before Checking for Duplicate Fix

--- app/src/main/java/com/example/tutorlogin/AddTutor.java
+++ app/src/main/java/com/example/tutorlogin/AddTutor.java
@@ -7,11 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,12 +24,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
 public class AddTutor extends AppCompatActivity {
 
-    EditText tutorId, tutorName, tutorRole, tutorSubjects;
+    EditText tutorId, tutorName, tutorRole, tutorSubjects, tutorUserName, tutorPassword;
 
     Button addTutor;
 
+    FirebaseAuth firebaseAuth;
     FirebaseDatabase rootNode;
     DatabaseReference reference;
 
@@ -38,6 +46,16 @@ public class AddTutor extends AppCompatActivity {
         tutorName = findViewById(R.id.tutor_name);
         tutorRole = findViewById(R.id.tutor_role);
         tutorSubjects = findViewById(R.id.tutor_subject);
+        tutorUserName = findViewById(R.id.username);
+        tutorPassword = findViewById(R.id.password);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() != null) {
+            Intent i = new Intent(AddTutor.this, AdminHub.class);
+            startActivity(i);
+            finish();
+        }
 
         addTutor.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,18 +79,40 @@ public class AddTutor extends AppCompatActivity {
 
     private boolean addOne(TutorModel tutorModel) {
         boolean success = false;
+        String userName = tutorUserName.getText().toString().trim();
+        String password = tutorPassword.getText().toString().trim();
+
         if(!tutorModel.getName().isEmpty() &&
                 !tutorModel.getId().isEmpty() &&
                 !tutorModel.getRole().isEmpty() &&
-                !tutorModel.getSubject().isEmpty()) {
+                !tutorModel.getSubject().isEmpty() &&
+                !TextUtils.isEmpty(userName) &&
+                !TextUtils.isEmpty(password)) {
 
             rootNode = FirebaseDatabase.getInstance();
             reference = rootNode.getReference("Tutor");
 
             String keyValue = reference.child(tutorModel.getId()).getKey();
+
+            
             if(!(keyValue !=null && keyValue.equals(tutorModel.getId()))) {
                 reference.child(tutorModel.getId()).setValue(tutorModel);
                 success = true;
+
+                // register the user in firebase
+
+                firebaseAuth.createUserWithEmailAndPassword(userName, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(AddTutor.this, "Tutor Created!", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(AddTutor.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
             }
         }
 
--- app/src/main/java/com/example/tutorlogin/TutorLogin.java
+++ app/src/main/java/com/example/tutorlogin/TutorLogin.java
@@ -1,22 +1,39 @@
 package com.example.tutorlogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class TutorLogin extends AppCompatActivity implements View.OnClickListener{
 
+    TextView username, password;
     Button submitButton;
+
+    FirebaseAuth fAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor_login);
 
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
         submitButton = findViewById(R.id.login);
+        fAuth = FirebaseAuth.getInstance();
 
         submitButton.setOnClickListener(this);
 
@@ -25,7 +42,48 @@ public class TutorLogin extends AppCompatActivity implements View.OnClickListene
     @Override
     public void onClick(View v)
     {
-        Intent i = new Intent(this,TutorHub.class);
-        startActivity(i);
+        final String recievedUsername = username.getText().toString().trim();
+        String recievedPassword = password.getText().toString().trim();
+
+        if(recievedUsername.equals("admin")) {
+            Intent intent = new Intent(TutorLogin.this,AdminHub.class);
+            startActivity(intent);
+            finish();
+        }
+
+        if(TextUtils.isEmpty(recievedUsername)) {
+            Toast.makeText(this, "Username is Required.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(recievedPassword)) {
+            Toast.makeText(this, "Password is Required.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        fAuth.signInWithEmailAndPassword(recievedUsername, recievedPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(TutorLogin.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent;
+                    if(recievedUsername.equals("admin")) {
+                        intent = new Intent(TutorLogin.this,AdminHub.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        intent = new Intent(TutorLogin.this,TutorHub.class);
+                    }
+
+                    startActivity(intent);
+                } 
+                else {
+                    Toast.makeText(TutorLogin.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
     }
 }
